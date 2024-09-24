976b38d6800a0543a3fbd5e1a03c049ab4712310
BelkoRees <rubikubi2023@gmail.com>
2024-09-24 22:25:02 +0700

    another one commit

---
+++ js/js/main.js
@@ -0,0 +1,31 @@
+document.writeln("/////////");
+document.writeln("МНОГО ХИ-ХИ-ХИ-ХИ-ХИ");
+console.log('Немного хи-хи в консоль');
+console.info('Тоже хи-хи');
+console.error('Ошибочное хи-хи');
+console.warn('Предупреждающее хи-хи');
+
+
+var name = prompt("Как тебя зовут?");
+
+if (confirm("Хочешь много хи-хи?")) {
+    if (confirm("Хочешь хи-хи ещё?")) {
+        alert("Ещё?");
+        alert("Ещё?");
+        alert("Больше хи-хи?");
+        alert("Ещё?");
+        alert("Ещё больше хи-хи?!");
+        alert("Ладно, ты заслужил много хи-хи!");
+        var num = 1000;
+        while (num > 0) {
+            document.writeln("--ХИ-ХИ-ХИ-ХИ-ХИ");
+            num--;
+        }
+    }
+    else {
+        alert("Ну ты и кринжулькин :(");
+    }
+} else {
+    alert("На нет и суда нет!");
+}
+
