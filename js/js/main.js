49d927e7863889fcb0369296d496506701605e40
BelkoRees <rubikubi2023@gmail.com>
2024-09-25 22:00:36 +0700

    another one commit

--- js/js/main.js
+++ js/js/main.js
@@ -1,12 +1,7 @@
-document.writeln("/////////");
-document.writeln("МНОГО ХИ-ХИ-ХИ-ХИ-ХИ");
-console.log('Немного хи-хи в консоль');
+/* console.log('Немного хи-хи в консоль');
 console.info('Тоже хи-хи');
 console.error('Ошибочное хи-хи');
 console.warn('Предупреждающее хи-хи');
-
-
-var name = prompt("Как тебя зовут?");
 
 if (confirm("Хочешь много хи-хи?")) {
     if (confirm("Хочешь хи-хи ещё?")) {
@@ -27,5 +22,43 @@ if (confirm("Хочешь много хи-хи?")) {
     }
 } else {
     alert("На нет и суда нет!");
-}
+} */
 
+var text = document.getElementById('textik');
+text.innerHTML = "Опять хи-хи???";
+
+document.getElementById('main-form').addEventListener('submit', checkForm);
+
+function checkForm(event) {
+    event.preventDefault();
+    var el = document.getElementById('main-form');
+
+    var name = el.name.value;
+    var pass = el.pass.value;
+    var repass = el.repass.value;
+    var state = el.state.value;
+
+    var error = '';
+    if(name == '' || pass == '' || state == '')
+        error = "Заполните все поля!";
+    else if(name.length <= 1 || name.length > 20)
+        error = "Введите норм имя";
+    else if(pass != repass)
+        error = "Пароли должна совпадать!";
+    else if(pass.split("@").length > 1)
+        error = "Мы не любим собак!";
+    else if(pass.split("3").length > 1)
+        error = "Мы не любим цифру 3!";
+    else if(pass.split("8").length > 1)
+        error = "Мы не любим цифру 8!";
+    else if(pass.split("p").length > 1)
+        error = "Мы не любим букву p!";
+
+    if (error != "") {
+        document.getElementById('error').innerHTML = error;
+
+    } else {
+        alert("Все данные корректно заполнены!");
+        window.location = 'https://obzorposudy.ru/polezno/cto-znacit-fraza-menya-zaskamili';
+    }
+}
